9b0b258db3c6e1f2210cfb968be94ef62d00c39f
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-15 20:52:23 +0900

    基本的な型の学習、関数の引数に型を指定してみた

---
+++ src/typeList.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// boolean
+let bool: boolean = true;
+
+// number
+let num: number = 1;
+
+// string
+let str: string = "A";
+
+// array
+let arr1: Array<number> = [1];
+
+// object
+let obj1: object = { A: 1 };
+let obj2: { id: number; name: string } = { id: 1, name: "name" };
+
+// any
+let any: any = 123;
+
+// null
+let null1: null = null;
+
+// undefined
+let undefined1: undefined = undefined;
+
+let funcA = (): void => {
+  return;
+};
