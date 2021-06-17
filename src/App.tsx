0a01dfe7e95268e3ba19dc9c1973d741fb6d9ed7
koki-noguchi <67818050+koki-noguchi@users.noreply.github.com>
2021-06-18 07:47:08 +0900

    カスタムフックを試した

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import { useState } from "react";
 import { Todo } from "./Todo";
 import { todoType } from "./types/todo";
 import { Text } from "./Text";
-import { UserForCard } from "./types/api/userForCard";
+import { useAllUsers } from "./hooks/useAllUsers";
 
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
-import { UserProfile } from "./types/userProfile";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<todoType>>([]);
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
+  const { getUsers, error, loading, userProfiles } = useAllUsers();
   const onClickFetchData = () => {
     axios
       .get<Array<todoType>>("https://jsonplaceholder.typicode.com/todos")
@@ -22,26 +18,7 @@ export default function App() {
         setTodos(res.data);
       });
   };
-  const onClickFetchUser = () => {
-    setLoading(true);
-    axios
-      .get<Array<UserForCard>>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        const data = res.data.map((user) => ({
-          id: user.id,
-          name: `${user.name}.(${user.username})`,
-          email: user.email,
-          address: `${user.address.city}${user.address.suite}${user.address.street}`
-        }));
-        setUserProfiles(data);
-      })
-      .catch(() => {
-        setError(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
+  const onClickFetchUser = () => getUsers();
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データを取得</button>
